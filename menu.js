db5c3db64284697ab1668e449b810747a8df2a0e
Igor_g <igor.gnatiuk111@gmail.com>
2025-12-10 20:18:35 +0200

    лендінг StarBucks

---
+++ menu.js
@@ -0,0 +1,23 @@
+  const contEl = document.querySelector(".container");
+
+
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileLink = document.querySelectorAll("[data-mobile-fix]");
+
+  [...mobileLink].map((elem) =>
+    elem.addEventListener("click", () => {
+      mobileMenuRef.classList.remove("is-open");
+      menuBtnRef.classList.remove("is-open");
+    })
+  );
+
+  menuBtnRef.addEventListener("click", () => {
+    menuBtnRef.classList.toggle("is-open");
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
+
+
